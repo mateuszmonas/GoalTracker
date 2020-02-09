a73a15a7c0c2195e728e14a94a1494bcff619479
agga1 <agud100@gmail.com>
2020-02-10 00:15:30 +0100

    [update] goal has all necessary attributes

--- app/src/main/java/com/agh/goaltracker/addgoal/ui/addgoal/AddGoalFragment.java
+++ app/src/main/java/com/agh/goaltracker/addgoal/ui/addgoal/AddGoalFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.agh.goaltracker.GoalTrackerApplication;
@@ -21,6 +24,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
@@ -29,6 +33,10 @@ public class AddGoalFragment extends Fragment {
 
     @BindView(R.id.goal_name_txt)
     EditText goalName;
+    private boolean countAsMinutes=true;
+    private int goal = 0;
+    Date chosenDate=null;
+
     private AddGoalViewModel addGoalViewModel;
     private Unbinder unbinder;
 
@@ -66,13 +74,36 @@ public class AddGoalFragment extends Fragment {
             Toast.makeText(getContext(), "Goal name cannot be empty!", Toast.LENGTH_SHORT).show();
         } else {
             String name = goalName.getText().toString();
-            Date currentTime = Calendar.getInstance().getTime();
-            addGoalViewModel.saveGoal(new Goal(name, currentTime));
+            addGoalViewModel.saveGoal(new Goal(name, chosenDate, countAsMinutes, goal));
             Toast.makeText(
                     getContext(),
                     "saved!",
                     Toast.LENGTH_SHORT).show();
             getActivity().finish();
+        }
+    }
+
+    @OnClick(R.id.choose_date)
+    public void setDueDate() { // TODO choose date from calendar
+        Toast.makeText(getContext(), "display calendar", Toast.LENGTH_SHORT).show();
+        chosenDate = Calendar.getInstance().getTime();
+    }
+
+    @OnClick(R.id.set_goal)
+    public void setGoal() { // TODO set goal
+        Toast.makeText(getContext(), countAsMinutes ? "display hour and minute chooser popup": "display popup to enter nr of events", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick({R.id.radio_as_events, R.id.radio_as_min})
+    public void onRadioButtonClicked(RadioButton radioButton) {
+        boolean checked = radioButton.isChecked();
+        switch (radioButton.getId()) {
+            case R.id.radio_as_events:
+                if (checked) countAsMinutes = false;
+                break;
+            case R.id.radio_as_min:
+                if (checked) countAsMinutes = true;
+                break;
         }
     }
 
--- app/src/main/java/com/agh/goaltracker/model/Goal.java
+++ app/src/main/java/com/agh/goaltracker/model/Goal.java
@@ -19,15 +19,30 @@ public class Goal {
     @ColumnInfo(name = "due_date")
     public Date dueDate;
 
+    @ColumnInfo(name = "progress_type_as_minutes")
+    public boolean progressAsMinutes;
+
+    @ColumnInfo(name = "current_progress")
+    public int currentProgress ;
+
+    @ColumnInfo(name = "total_goal")
+    public int totalGoal;
+
     @Ignore
     public Goal(String title) {
-        this.title = title;
-        this.dueDate = null;
+        this(title, null);
+    }
+    @Ignore
+    public Goal(String title, Date dueDate) {
+        this(title, dueDate, false, 0);
     }
 
-    public Goal(String title, Date dueDate) {
+    public Goal(String title, Date dueDate, boolean progressAsMinutes, int totalGoal) {
         this.title = title;
         this.dueDate = dueDate;
+        this.progressAsMinutes = progressAsMinutes;
+        this.currentProgress = 0;
+        this.totalGoal = totalGoal;
     }
 
     public String getTitle() {
@@ -40,5 +55,17 @@ public class Goal {
 
     public Date getDueDate() {
         return dueDate;
+    }
+
+    public boolean isProgressAsMinutes() {
+        return progressAsMinutes;
+    }
+
+    public int getCurrentProgress() {
+        return currentProgress;
+    }
+
+    public int getTotalGoal() {
+        return totalGoal;
     }
 }
